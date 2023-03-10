0c3709275c0f3db5135cb6f28505e177051ad64f
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-10 03:02:24 +0100

    BrandDiv

--- components/BrandDiv.tsx
+++ components/BrandDiv.tsx
@@ -4,10 +4,10 @@ export default function BrandDiv() {
   return (
 
 <>
-<h3 className="mb-4 mt-5 p-10 text-4xl font-extrabold leading-none text-center tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to the community of aesthetics</h3>
+<p className="mb-4 mt-5 p-10 text-1xl font-extrabold leading-7 text-center tracking-tight text-gray-900   dark:text-white">Welcome to our alternative fashion startup! Celebrate non-conformity, creativity, and self-expression through our curated collection of sustainable and ethical fashion. From vintage to goth, punk to bohemian, embrace your unique style with us.</p>
 
 <div className="flex justify-center">
-<a href="#" className="inline-flex text-center items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+<a href="#" className="inline-flex text-center items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-black rounded-lg hover:bg-black-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
     Signup as a brand
     <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 </a>
