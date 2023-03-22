6d984b0f8a6a2eb3d41f8808c62c9b54c148c6b4
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-22 03:41:12 +0100

    Added buttons for /accounts/

--- pages/accounts/login.tsx
+++ pages/accounts/login.tsx
@@ -32,13 +32,13 @@ export default function login() {
                             <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <a href="#" className="text-sm font-medium text-white hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
                   <button type="submit" className="w-full text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center  items-center dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200 mr-2 mb-2">
 Sign in
 </button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                      Don’t have an account yet? <a href="/accounts/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
               </form>
           </div>
--- pages/accounts/signup.tsx
+++ pages/accounts/signup.tsx
@@ -42,7 +42,7 @@ Create an account
                 
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Already have an account? 
-                      <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                      <a href="/accounts/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                   </p>
               </form>
           </div>
