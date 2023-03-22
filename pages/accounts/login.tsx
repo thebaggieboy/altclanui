1f444178fdf8810a175143fef097dee6f783784a
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-22 03:43:58 +0100

    Added logo on top of accounts page

--- pages/accounts/login.tsx
+++ pages/accounts/login.tsx
@@ -6,7 +6,7 @@ export default function login() {
         <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="/alteclan_logo.jpg" alt="logo"/>
+          <img className="w-8 h-8 mr-2 rounded" src="/alteclan_logo.jpg" alt="logo"/>
           Altclan    
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
