70f10b165ba9fca2290ffdbc42d5a1318c37e1e5
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-28 20:34:53 +0100

    Removed padding y from brandDiv

--- components/brands/BrandDiv.tsx
+++ components/brands/BrandDiv.tsx
@@ -7,19 +7,19 @@ export default function BrandDiv() {
     <>
     <br />
       <div className="bg-#ddd">
-        <p className="text-center  mx-auto  text-black p-10 sm:text-2xl">
+        <p className="text-center  mx-auto  text-black p-10 sm:text-4xl">
           Welcome to our alternative fashion startup! Celebrate non-conformity,
           creativity, and self-expression through our curated collection of
           sustainable and ethical fashion. From vintage to goth, punk to
           bohemian, embrace your unique style with us.
         </p>
- <br />
+
         <div className="grid place-items-center">
           <Link
             href="/brands/signup"
             className="flex items-center justify-center gap-x-2 px-5 py-3 text-base font-medium text-center bg-black rounded-lg text-white hover:bg-black-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
-            <span>Signup as a brand</span> 
+            <span>Signup as a brand</span>  <br />
            
           </Link>
         </div>
