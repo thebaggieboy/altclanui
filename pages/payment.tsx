012fa0373a84b753be8f07875be5441572a040ae
Enimofe Odujirin <daemonargo@protonmail.com>
2023-07-25 14:32:27 +0100

    Changed the checkout system

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Checkout() {
+export default function Payment() {
   return (
     <>
     <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
@@ -15,13 +15,15 @@ export default function Checkout() {
             </a>
             <span className="font-semibold text-gray-900">Shop</span>
           </li>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-          </svg>
+
           <li className="flex items-center space-x-3 text-left sm:space-x-4">
-            <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="/shipping">2</a>
+            <a className="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700" href="/shipping"
+              ><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" /></svg>
+            </a>
             <span className="font-semibold text-gray-900">Shipping</span>
           </li>
+
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
           </svg>
@@ -33,55 +35,9 @@ export default function Checkout() {
       </div>
     </div>
   </div>
-  <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
-    <div className="px-4 pt-8">
-      <p className="text-xl font-medium">Order Summary</p>
-      <p className="text-gray-400">Check your items. And select a suitable shipping method.</p>
-      <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
-        <div className="flex flex-col rounded-lg bg-white sm:flex-row">
-          <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-          <div className="flex w-full flex-col px-4 py-4">
-            <span className="font-semibold">Nike Air Max Pro 8888 - Super Light</span>
-            <span className="float-right text-gray-400">42EU - 8.5US</span>
-            <p className="text-lg font-bold">$138.99</p>
-          </div>
-        </div>
-        <div className="flex flex-col rounded-lg bg-white sm:flex-row">
-          <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-          <div className="flex w-full flex-col px-4 py-4">
-            <span className="font-semibold">Nike Air Max Pro 8888 - Super Light</span>
-            <span className="float-right text-gray-400">42EU - 8.5US</span>
-            <p className="mt-auto text-lg font-bold">$238.99</p>
-          </div>
-        </div>
-      </div>
+
   
-      <p className="mt-8 text-lg font-medium">Shipping Methods</p>
-      <form className="mt-5 grid gap-6">
-        <div className="relative">
-          <input className="peer hidden" id="radio_1" type="radio" name="radio" checked />
-          <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
-          <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_1">
-            <img className="w-14 object-contain" src="/images/naorrAeygcJzX0SyNI4Y0.png" alt="" />
-            <div className="ml-5">
-              <span className="mt-2 font-semibold">Fedex Delivery</span>
-              <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
-            </div>
-          </label>
-        </div>
-        <div className="relative">
-          <input className="peer hidden" id="radio_2" type="radio" name="radio" checked />
-          <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
-          <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_2">
-            <img className="w-14 object-contain" src="/images/oG8xsl3xsOkwkMsrLGKM4.png" alt="" />
-            <div className="ml-5">
-              <span className="mt-2 font-semibold">Fedex Delivery</span>
-              <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
-            </div>
-          </label>
-        </div>
-      </form>
-    </div>
+    
     <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
       <p className="text-xl font-medium">Payment Details</p>
       <p className="text-gray-400">Complete your order by providing your payment details.</p>
--- pages/shipping.tsx
+++ pages/shipping.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Checkout() {
+export default function Shipping() {
   return (
     <>
     <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
@@ -15,13 +15,15 @@ export default function Checkout() {
             </a>
             <span className="font-semibold text-gray-900">Shop</span>
           </li>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-          </svg>
+
           <li className="flex items-center space-x-3 text-left sm:space-x-4">
-            <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="/shipping">2</a>
+            <a className="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700" href="/shipping"
+              ><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" /></svg>
+            </a>
             <span className="font-semibold text-gray-900">Shipping</span>
           </li>
+
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
           </svg>
