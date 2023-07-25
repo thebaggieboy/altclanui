e6fc78eeaad2fd312b05011b5c32e5aa320ccac4
Enimofe Odujirin <daemonargo@protonmail.com>
2023-07-25 14:00:24 +0100

    Changed the search bar

--- pages/search.jsx
+++ pages/search.jsx
@@ -42,23 +42,34 @@ export default function Search() {
   return (
    
   <div>
-    <HeaderTab/>
-      <div className="p-5 mx-auto mt-2 ">
+      <div className="p-10 mx-auto mt-2 ">
 
-      <form>   
-    <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div className="relative">
-      
-        <div>
-              <input type="text"  id="email" className="shadow-sm bg-gray-50 border p-3 rounded w-full border-gray-300 shadow-sm" placeholder="Brands, Merchandises ..." required />
-          </div>
+<form className="flex items-center">   
+    <label for="simple-search" className="sr-only">Search</label>
+    <div className="relative w-full">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
+            </svg>
+        </div>
+        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search brand name, products..." required/>
     </div>
+    <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-dark bg-black rounded-lg border border-black hover:bg-white focus:ring-4 focus:outline-none focus:ring-black">
+        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+        <span className="sr-only">Search</span>
+    </button>
 </form>
 
 
-<Category/>
 
     </div>
+
+    <div className="search-results">
+
+    </div>
+    
   </div>
   )
 }
