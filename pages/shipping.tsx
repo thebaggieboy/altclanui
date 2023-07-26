51ea7ee52c6b0540d14cf5eac720ccf7a08375a7
Enimofe Odujirin <daemonargo@protonmail.com>
2023-07-26 06:57:41 +0100

    Solved JSX ending tag issue and shipping page

--- pages/shipping.tsx
+++ pages/shipping.tsx
@@ -65,7 +65,7 @@ export default function Shipping() {
           <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" htmlFor="radio_1">
             <img className="w-14 object-contain" src="/images/naorrAeygcJzX0SyNI4Y0.png" alt="" />
             <div className="ml-5">
-              <span className="mt-2 font-semibold">Fedex Delivery</span>
+              <span className="mt-2 font-semibold">Standard Delivery</span>
               <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
             </div>
           </label>
@@ -76,7 +76,7 @@ export default function Shipping() {
           <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" htmlFor="radio_2">
             <img className="w-14 object-contain" src="/images/oG8xsl3xsOkwkMsrLGKM4.png" alt="" />
             <div className="ml-5">
-              <span className="mt-2 font-semibold">Fedex Delivery</span>
+              <span className="mt-2 font-semibold">Express Delivery</span>
               <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
             </div>
           </label>
@@ -84,8 +84,8 @@ export default function Shipping() {
       </form>
     </div>
     
-    <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
-      <p className="text-xl font-medium">Payment Details</p>
+    <div className="mt-10 bg-gray-50 px-4 pt-6 lg:mt-0">
+      <p className="text-xl font-medium">Shipping Details</p>
       <p className="text-gray-400">Complete your order by providing your payment details.</p>
       <div className="">
         <label htmlFor="email" className="mt-4 mb-2 block text-sm font-medium">Email</label>
@@ -94,6 +94,24 @@ export default function Shipping() {
           <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+            </svg>
+          </div>
+        </div>
+        <label htmlFor="first-name" className="mt-4 mb-2 block text-sm font-medium">First Name</label>
+        <div className="relative">
+          <input type="text" id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your full name here" />
+          <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
+            </svg>
+          </div>
+        </div>
+        <label htmlFor="last-name" className="mt-4 mb-2 block text-sm font-medium">Last Name</label>
+        <div className="relative">
+          <input type="text" id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your full name here" />
+          <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
             </svg>
           </div>
         </div>
