b234cc48d24422a6de9dd31ac849d060147cefb5
Enimofe Odujirin <daemonargo@protonmail.com>
2023-08-19 09:09:59 -0500

    updated brand signup form

--- pages/brands/register/index.jsx
+++ pages/brands/register/index.jsx
@@ -9,7 +9,7 @@ export default function signup() {
         <span className="flex items-center justify-center w-5 h-5 mr-2 text-xs border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
             1
         </span>
-        Login <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+        Account <span className="hidden sm:inline-flex sm:ml-2">Info</span>
         <svg className="w-3 h-3 ml-2 sm:ml-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 9 4-4-4-4M1 9l4-4-4-4"/>
         </svg>
@@ -18,7 +18,7 @@ export default function signup() {
         <span className="flex items-center justify-center w-5 h-5 mr-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
             2
         </span>
-        Brand <span className="hidden sm:inline-flex sm:ml-2">Bio</span>
+        Brand Bio<span className="hidden sm:inline-flex sm:ml-2"></span>
         <svg className="w-3 h-3 ml-2 sm:ml-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 9 4-4-4-4M1 9l4-4-4-4"/>
         </svg>
@@ -27,7 +27,7 @@ export default function signup() {
         <span className="flex items-center justify-center w-5 h-5 mr-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
             3
         </span>
-        Contact
+        Contact 
     </li>
 </ol>
 <br/>
