2c08c6b521676a28d33bc0133088b0243180d1a9
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-08-23 23:39:01 +0100

    dd

--- components/BrandSignUpForm.jsx
+++ components/BrandSignUpForm.jsx
@@ -7,7 +7,7 @@ export default function BrandSignup() {
       
 <section className="p-10">
 
-<form className="p-5">
+<form className="">
   <div className="mb-6">
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
     <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
@@ -25,7 +25,7 @@ export default function BrandSignup() {
  
   <button
             type="submit" 
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-black bg-black py-3 px-8 text-base font-medium text-dark  focus:ring-black focus:ring-offset-2"
+                className="mt-2 flex w-full items-center justify-center rounded-md border border-black bg-black py-3 px-8 text-base font-medium text-dark  focus:ring-black focus:ring-offset-2"
               >
                 Create Account
               </button>
