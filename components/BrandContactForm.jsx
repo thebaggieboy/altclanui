74483bab6d6b66d323c8816d3da996bc3f7209f1
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-08-23 23:59:18 +0100

    dd

--- components/BrandContactForm.jsx
+++ components/BrandContactForm.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
-
+import Link from 'next/link'
 export default function BrandContactForm() {
   return (
-    <div>BrandContactForm</div>
+    <div className="w-100 p-20 ">
+<section className="p-10">
+
+<form className="p-5">
+  <div className="mb-6">
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile Number</label>
+    <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+  </div>
+  <div className="mb-6">
+  
+<label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Billing Address</label>
+<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+  </div>
+
+  <div className="mb-6">
+    <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
+    <input type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
+  </div>
+  <div className="mb-6">
+    <label htmlFor="state" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">State</label>
+    <input type="text" id="state" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
+  </div>
+  <div className="mb-6">
+    <label htmlFor="zip" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zip</label>
+    <input type="text" id="zip" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
+  </div>
+
+
+ 
+  <button
+            type="submit" 
+                className="mt-10 flex w-full items-center justify-center rounded-md border border-black bg-black py-3 px-8 text-base font-medium text-dark  focus:ring-black focus:ring-offset-2"
+              >
+                Continue
+              </button>
+</form>
+    
+</section>
+
+    </div>
   )
 }
