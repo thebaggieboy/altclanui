c7d85703851ca4e2b673ec6d8c54186ce3ef9677
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-08-25 18:10:48 +0100

    added context and reducers

--- components/BrandContactForm.jsx
+++ components/BrandContactForm.jsx
@@ -4,7 +4,7 @@ import BrandSignupTab from "@/components/BrandSignupTab"
 
 export default function BrandContactForm() {
   return (
-    <div className="w-100 p-20 ">
+    <div className="w-100 ">
 <section className="p-10">
 
 <form className="p-5">
@@ -13,10 +13,8 @@ export default function BrandContactForm() {
     <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
   </div>
   <div className="mb-6">
-  
-<label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Billing Address</label>
-<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-
+    <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Billing Address</label>
+    <input type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
   </div>
 
   <div className="mb-6">
@@ -38,12 +36,13 @@ export default function BrandContactForm() {
             type="submit" 
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-black bg-black py-3 px-8 text-base font-medium text-dark  focus:ring-black focus:ring-offset-2"
               >
-                Continue
+                Complete Signup
               </button>
 </form>
-    
+  
 </section>
 
     </div>
-  )
+  ) 
 }
+   
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,19 @@ import '@/styles/globals.css'
 import '../styles/main.scss'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import {BrandContextProvider} from '@/context/BrandContext'
 import {ProductsContextProvider} from '@/context/ProductContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   
   return(
+    <>
+      <BrandContextProvider>
         <Layout>
-          <ProductsContextProvider>
-        
-          <Component {...pageProps} />
-          </ProductsContextProvider>
+        <Component {...pageProps} />
         </Layout>
+      </BrandContextProvider>
+    </>
       
   ) 
 }
