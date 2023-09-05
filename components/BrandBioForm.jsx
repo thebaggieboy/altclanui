ed90c29c52f2179b4205e8fe0abd3f009da73f5b
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-09-05 12:15:53 +0100

    eEEBE

--- components/BrandBioForm.jsx
+++ components/BrandBioForm.jsx
@@ -12,20 +12,33 @@ export default function BrandSignUpForm() {
       
       <form className="">
         <div className="mb-6">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">What is the name of your brand?</label>
+
+          <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand Username</label>
+          <input type="text" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name of Brand</label>
           <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
         </div>
         <div className="mb-6">
+        
         
       <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Brand Bio</label>
       <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
       
         </div>
-      
-        <div className="mb-6">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@brand.com" required />
-        </div>
+   
+<label for="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your brand type</label>
+<select id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  <option>Clothing & Apparel</option>
+  <option>Accessories</option>
+  <option>Footwears & Slides</option>
+  <option>Enigmas</option>
+  <option>Wristwatches & Handbands</option>
+  <option>Art</option>
+  <option>Skates & Boards</option>
+</select>
+
       
       
        
