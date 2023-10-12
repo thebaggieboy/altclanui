af64d680796827d045a5248e77d2d58a7c65a434
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-10-12 12:33:34 +0100

    fff

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -340,7 +340,7 @@ export default function Products({_id, merchandise_name, price, picture}) {
             
 <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
+        <img class="p-8 rounded-t-lg" src="/black-tee.jpg" alt="" />
     </a>
     <div class="px-5 pb-5">
         <a href="#">
@@ -365,8 +365,8 @@ export default function Products({_id, merchandise_name, price, picture}) {
             <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
         </div>
         <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
-            <button onClick={addToCart} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
+            <span class="text-2xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+            <button onClick={addToCart} class="text-white bg-black font-medium rounded-sm text-sm px-5 py-2.5 text-center">Add to cart</button>
         </div>
     </div>
 </div>
