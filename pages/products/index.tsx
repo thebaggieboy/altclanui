a85600cadb77c919e99a0c634db5812b33df1c9b
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-10-15 21:39:09 +0100

    13/10/2023

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -368,8 +368,8 @@ export default function Products({_id, merchandise_name, price, picture}) {
             <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
         </div>
         <div class="flex items-center justify-between">
-            <span class="text-2xl font-bold text-gray-900 dark:text-white">${product.price}</span>
-            <button onClick={addToCart} class="text-white bg-black font-medium rounded-sm text-sm px-5 py-2.5 text-center">Add to cart</button>
+            <span class="lead text-gray-700 dark:text-white">${product.price}</span>
+            <button onClick={addProductToCart} class=" flex  items-center justify-center rounded-md border p-3 border-black bg-black text-base font-medium text-white text-sm  focus:ring-black focus:ring-offset-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5"><path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"></path></svg></button>
         </div>
     </div>
 </div>
