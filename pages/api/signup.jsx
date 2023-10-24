5aa13e587354cd0675af7a5a8e26d4996e02ecc5
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-10-24 03:06:14 +0100

    hASH

--- pages/api/signup.jsx
+++ pages/api/signup.jsx
@@ -7,50 +7,49 @@ import bcrypt from "bcrypt"
 
 export default function handler(req, res) {
 	//const externalApiUrl = "http://127.0.0.1:8000/api/users/";
-	const externalApiUrl = useBrands('https://altclan-api-v1.onrender.com/api/users/')
+	const externalApiUrl = 'https://altclan-api-v1.onrender.com/api/users/'
   
-	const {email, password} = req.body
-	
+	let {email, password} = req.body
 	const saltRounds = 10;
 
-	function hashPassword(password){
-		bcrypt
-		.hash(password, saltRounds)
-		.then(hash => {
-		  console.log(`Hash: ${hash}`);
-		  // Store hash in your password DB.
-		  //password = hash
-		  console.log("New password hash: ", hash )	
-	  
-		})
-		.catch(err => console.error(err.message));
-	}
+bcrypt
+  .hash(password, saltRounds)
+  .then(hash => {
+    console.log(`Hash: ${hash}`);
+    // Store hash in your password DB.
+	const setCookie = (name, value, days) => {
+		const expires = new Date();
+		expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+		const cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;httpOnly;`
+		res.setHeader('Set-Cookie', cookie);
+	};
+	
+	// Sign the user credentials
+	//password = hash
+	const token = jwt.sign({ email, hash }, "secretkey", { expiresIn: "24h" });
+	
+	setCookie("token", token)
+	console.log('Token: ', token)
+	console.log("Current password value: ", password)
+	
+	res.status(200).json({ token })
+	
+	  fetch(externalApiUrl, {
+		method: 'POST',
+		headers: {
+			
+			'Content-Type': 'application/json',
+	 
+		},
+		body:JSON.stringify({email, password:hash, token})
+	})
 
+
+  })
+  .catch(err => console.error(err.message));
+	
 
 	
-  const setCookie = (name, value, days) => {
-	const expires = new Date();
-	expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-	const cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;httpOnly;`
-	res.setHeader('Set-Cookie', cookie);
-};
-
-// Sign the user credentials
-const token = jwt.sign({ email, password }, "secretkey", { expiresIn: "24h" });
-
-setCookie("token", token)
-console.log('Token: ', token)
-
-res.status(200).json({ token })
-
-  fetch(externalApiUrl, {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json',
- 
-	},
-	body:JSON.stringify({email, password, token})
-})
 
 }
 
