f21d447598d012f9d205ae9380aaab2d7655af23
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-10-28 12:53:07 +0100

    alert for signup duplicate user

--- pages/accounts/signup.jsx
+++ pages/accounts/signup.jsx
@@ -8,6 +8,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectUser, setUser } from "../../features/user/userSlice";
 import signUp from "../../lib/signUp";
 
+export  function LoginError(){
+	return (
+		<div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+  <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+  </svg>
+  <span class="sr-only">Info</span>
+  <div class="ml-3 text-sm font-medium">
+    You already have an account with us  <a href="/accounts/login" class="font-semibold underline hover:no-underline">Login</a> to continue.
+  </div>
+  <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-2" aria-label="Close">
+    <span class="sr-only">Close</span>
+    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+    </svg>
+  </button>
+</div>
+	)
+}
+
 export default function SignUp() {
 	const dispatch = useDispatch();
 	const user = useSelector(selectUser);
@@ -20,6 +40,8 @@ export default function SignUp() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [error, setError] = useState(null);
+
+	
 
 	const emailErr = error?.email[0] || null;
 
@@ -64,10 +86,10 @@ export default function SignUp() {
 						<h1 className={styles.greeting}>Welcome to Altclan</h1>
 						<p className={styles.login}>Complete your sign up to join</p>
 
-						<div>
+						<div className="">
 							{/* <label for="email" className="block mb-2 text-sm font-medium text-black">Your email</label> */}
 							{emailErr !== null && (
-								<p className="pl-16 text-lg md:text-[.8rem] text-red-800">{emailErr}</p>
+							<LoginError/>
 							)}
 							<input
 								type="email"
