5fa998e4b1e18ae603304358d5a738b86b7f1f99
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-08 13:45:16 +0100

    Added new profile heading

--- components/headers/HeaderNav.jsx
+++ components/headers/HeaderNav.jsx
@@ -556,27 +556,6 @@ export default function HeaderNav() {
 									</Link>
 								</div>
 
-						
-
-								{/* Cart */}
-								<div className="ml-4 flow-root lg:ml-6">
-									<Link
-										href="/cart"
-										data-modal-target="defaultModal"
-										data-modal-toggle="defaultModal"
-										className="group -m-2 flex items-center p-2"
-									>
-										<ShoppingBagIcon
-											className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-											aria-hidden="true"
-										/>
-										<span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-											{cartCount}
-										</span>
-										<span className="sr-only">items in cart, view bag</span>
-									</Link>
-								</div>
-
 
 								 {/* Profile dropdown */}
 								 <Menu as="div" className="relative ml-3">
@@ -634,6 +613,28 @@ export default function HeaderNav() {
                     </Menu.Items>
                   </Transition>
                 </Menu>
+						
+
+								{/* Cart */}
+								<div className="ml-4 flow-root lg:ml-6">
+									<Link
+										href="/cart"
+										data-modal-target="defaultModal"
+										data-modal-toggle="defaultModal"
+										className="group -m-2 flex items-center p-2"
+									>
+										<ShoppingBagIcon
+											className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+											aria-hidden="true"
+										/>
+										<span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+											{cartCount}
+										</span>
+										<span className="sr-only">items in cart, view bag</span>
+									</Link>
+								</div>
+
+
 							</div>
 						</div>
 					</div>
