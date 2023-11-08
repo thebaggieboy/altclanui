272e15653185207200abc41c2fe3ac4599bba4a0
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-08 13:41:54 +0100

    Added new profile heading

--- components/headers/HeaderNav.jsx
+++ components/headers/HeaderNav.jsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 
 import { Fragment, useContext, useState } from "react";
-import { Dialog, Popover, Tab, Transition } from "@headlessui/react";
+import { Dialog, Popover, Tab, Transition,  Disclosure, Menu } from "@headlessui/react";
+
+
+
 import {
 	Bars3Icon,
 	MagnifyingGlassIcon,
@@ -553,20 +556,7 @@ export default function HeaderNav() {
 									</Link>
 								</div>
 
-									{/* Search */}
-								<div className="flex sm: block lg:hidden">
-									<Link href="/profile">
-										<div className="p-2 text-gray-400 hover:text-gray-500">
-											<span className="sr-only">Search</span>
-											
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-</svg>
-
-
-										</div>
-									</Link>
-								</div>
+						
 
 								{/* Cart */}
 								<div className="ml-4 flow-root lg:ml-6">
@@ -586,6 +576,64 @@ export default function HeaderNav() {
 										<span className="sr-only">items in cart, view bag</span>
 									</Link>
 								</div>
+
+
+								 {/* Profile dropdown */}
+								 <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="/img/profile.jpg"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
 							</div>
 						</div>
 					</div>
