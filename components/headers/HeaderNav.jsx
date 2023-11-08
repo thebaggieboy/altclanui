2a062d04fc13b31b1c782a7af04562c68c9c63cc
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-08 13:44:27 +0100

    Added new profile heading

--- components/headers/HeaderNav.jsx
+++ components/headers/HeaderNav.jsx
@@ -581,7 +581,7 @@ export default function HeaderNav() {
 								 {/* Profile dropdown */}
 								 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="relative lg:hidden flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
