742dbdc4830bc16a755dbb105b970b538f658031
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-11 01:13:45 +0100

    Restyled navbar

--- components/headers/HeaderNav.jsx
+++ components/headers/HeaderNav.jsx
@@ -580,7 +580,8 @@ export default function HeaderNav() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
+						{user === null ?
+						  <Menu.Item>
                         {({ active }) => (
                           <Link
                             href="/signup"
@@ -588,9 +589,26 @@ export default function HeaderNav() {
                           >
                             Sign up
                           </Link>
+							
+						  
                         )}
-                      </Menu.Item>
-                      <Menu.Item>
+                      </Menu.Item>:	
+					<Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/profile"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                           Profile
+                          </Link>
+							
+						  
+                        )}
+                      </Menu.Item> 
+					  
+					  }
+                     
+					  {user === null ?     <Menu.Item>
                         {({ active }) => (
                           <Link
                             href="/login"
@@ -599,7 +617,19 @@ export default function HeaderNav() {
                             Log in
                           </Link>
                         )}
-                      </Menu.Item>
+                      </Menu.Item> : 
+					      <Menu.Item>
+						  {({ active }) => (
+							<Link
+							  href=""
+							  onClick={logout}
+							  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-red')}
+							>
+							  Logout
+							</Link>
+						  )}
+						</Menu.Item>}
+                  
                   
                     </Menu.Items>
                   </Transition>
