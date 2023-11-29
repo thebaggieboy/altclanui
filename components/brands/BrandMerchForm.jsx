4c5b3885cf71683d3631c9c60b6411b448f35a1c
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-29 14:19:52 +0100

    Vercel analytics

--- components/brands/BrandMerchForm.jsx
+++ components/brands/BrandMerchForm.jsx
@@ -180,8 +180,58 @@ const BrandMerchForm = (props) => {
                        <option  value="M">M</option>
                        <option value="L">L</option>
                        <option value="XL">XL</option>
-                       <option value="XXL">XXL</option>
+                       <option value="2XL">2XL</option>
                        <option value="3XL">3XL</option>
+             
+                  </select> 
+               </div>
+
+               <div className=''>
+               <label htmlFor="" className={styles.label}> Ring Size Guide (Choose all available sizes)</label>
+                 
+                 <select multiple className={styles.input} onChange={inputChangeHandler} name="brand_type" id="">
+                 
+                       <option  value="3">3</option>
+                       <option value="3.5">3.5</option>
+                       <option value="4">4</option>
+                       <option value="4.5">4.5</option>
+                       <option value="5">5</option>
+                       <option  value="5.5">5.5</option>
+                       <option  value="6">6</option>
+                       <option  value="6.5">6.5</option>
+                       <option value="7">7</option>
+                       <option value="7.5">7.5</option>
+                       <option value="8">8</option>
+                       <option value="8.5">8.5</option>
+                       <option value="9">9</option>
+                       <option value="9.5">9.5</option>
+                       <option value="10">10</option>
+                       <option value="10.5">10.5</option>
+                       <option value="11">11</option>
+                       <option value="11.5">11.5</option>
+                       <option value="12">12</option>
+                       <option value="12.5">12.5</option>
+                       <option value="13">13</option>
+                       <option value="13.5">10.5</option>
+
+             
+                  </select> 
+               </div>
+
+               <div className=''>
+               <label htmlFor="" className={styles.label}>Shoe Size Guide (Choose all available sizes)</label>
+                 
+                 <select multiple className={styles.input} onChange={inputChangeHandler} name="brand_type" id="">
+                      
+                       <option  value="38">38</option>
+                       <option  value="39">39</option>
+                       <option value="40">40</option>
+                       <option value="41">41</option>
+                       <option value="42">42</option>
+                       <option value="43">43</option>
+                       <option value="44">44</option>
+                       <option value="45">45</option>
+                       <option value="46">46</option>
              
                   </select> 
                </div>
