a28f57955f08b6d6463c06ecaf62bbebda0b5f46
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-12-16 01:05:04 +0100

    Added label ui to product page

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -121,38 +121,31 @@ export default function ProductDetail({ _id, merch }) {
 				</nav>
 
 				{/* Image gallery */}
-				<div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-					<div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-						<img
-							src={merch.display_image}
-							alt=""
-							className="h-full w-full object-cover object-center"
-						/>
-					</div>
-					<div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-						<div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-							<img
-								src={merch.display_image}
-								alt=""
-								className="h-full w-full object-cover object-center"
-							/>
-						</div>
-						<div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-							<img
-								src={merch.display_image}
-								alt=""
-								className="h-full w-full object-cover object-center"
-							/>
-						</div>
-					</div>
-					<div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
-						<img
-							src={merch.display_image}
-							alt=""
-							className="h-full w-full object-cover object-center"
-						/>
-					</div>
-				</div>
+				
+
+<div class="grid gap-4 p-5">
+    <div>
+        <img class="h-auto max-w-full rounded-lg text-center" src={merch.display_image} alt=""/>
+    </div>
+    <div class="grid grid-cols-5 gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src={merch.display_image} alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src={merch.display_image} alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src={merch.display_image} alt="" />
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src={merch.display_image} alt=""  />
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src={merch.display_image} alt="" />
+        </div>
+    </div>
+</div>
+
 
 				{/* Product info */}
 				<div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
