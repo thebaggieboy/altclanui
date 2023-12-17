08a2facf47997b69b4c693364fd1cbd68789174e
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-12-17 06:27:12 +0100

    search bar

--- pages/search.jsx
+++ pages/search.jsx
@@ -54,7 +54,7 @@ export default function Search({ _id, merchandise_name, price, picture }) {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 
                 </div>
-                <input type="text" onChange={handleSearchChange} value={searchQuery} id="simple-search" className="bg-gray-50 p-5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="brand, merch, category" required/>
+                <input type="text" onChange={handleSearchChange} value={searchQuery} id="simple-search" className="bg-gray-50 mt-2   border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="brand, merch, category" required/>
             </div>
       
         </div>
