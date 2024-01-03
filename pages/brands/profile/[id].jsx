d14faf9fa5d6c716a2bbf40742090581409180b3
Mofe Odujirin <96820385+thebaggieboy@users.noreply.github.com>
2024-01-03 19:43:04 +0100

    Update [id].jsx

--- pages/brands/profile/[id].jsx
+++ pages/brands/profile/[id].jsx
@@ -23,8 +23,8 @@ export default function BrandProfile({ _id, merchandise_name, price, display_ima
     }, [brand_user]);
 
     const { data, isLoading, error } = useQuery({
-        queryKey: ["products", userId],
-        queryFn: () => fetchProductData(userId, true), enabled: brand_user !== null
+        queryKey: ["products"],
+        queryFn: () => fetchProductData()
     })
 
     console.log(data)
