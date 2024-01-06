81d7fa969d14e18be248f5c8a83571c322e29cf7
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-01-06 12:36:45 +0100

    s

--- pages/brands/login/index.jsx
+++ pages/brands/login/index.jsx
@@ -16,7 +16,6 @@ export default function Login(req, res) {
     const router = useRouter();
     const searchParams = useSearchParams()
 	const search = searchParams.get('user')
-
     const signupSuccess =    <div class="flex items-center text-center p-4 mb-4 text-sm text-green-800 border border-0 bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
     <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
@@ -31,7 +30,7 @@ export default function Login(req, res) {
 
     useLayoutEffect(() => {
         if (brand_user !== null) {
-            router.push(`/brands/profile/${brand_user?.id}`);
+            router.push(`/profile/${brand_user?.id}`);
         }
     }, [brand_user, router])
 
