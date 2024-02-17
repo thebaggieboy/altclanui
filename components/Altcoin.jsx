d182bccae47a1271b8b2a8047ea0dc5d78674418
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-02-18 00:24:27 +0100

    New UI adjustments

--- components/Altcoin.jsx
+++ components/Altcoin.jsx
@@ -10,7 +10,7 @@ export default function Altcoin() {
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-white">$ALC: A utility token</h1>
-        <p class="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        <p style={{fontFamily: 'Poppins'}} class="mb-8 text-sm font-normal text-white lg:text-sm sm:px-16 xl:px-48 dark:text-gray-400">
 Introducing $ALC, the revolutionary utility token designed to enhance your shopping experience across our platform! With $ALC, users unlock exclusive discounts, access special promotions, and enjoy seamless transactions while exploring a vast array of products. Stay tuned for the launch of $ALC and elevate your shopping journey to new heights!.</p>
         <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
--- components/Token.jsx
+++ components/Token.jsx
@@ -4,16 +4,16 @@ export default function Token(){
   return (
     <>
         
-        <section style={{backgroundColor: 'beige', padding: 20, margin:10}} class="flex items-center justify-center bg-white">
-  <div class="">
+        <section style={{backgroundColor: 'beige', padding: 20, fontFamily:"Poppins" }} class="flex items-center justify-center bg-white">
+  <div class="" style={{margin:10}}>
     <div class="text-center">
-      <p class="text-lg font-medium leading-8 font-bold text-black">Introducing Design Data Platform</p>
-      <h1 class="mt-3    tracking-tight text-gray-900">Distribute your brand from&nbsp;design to code</h1>
-      <p class="mt-3 text-lg leading-relaxed text-black">Specify helps you unify your brand identity by collecting, storing and distributing design tokens and assets — automatically.</p>
+      <p class="text-lg font-medium leading-8 font-bold text-black">Introducing Altclan Design Data Platform</p>
+      <p class="mt-3 text-sm leading-relaxed text-black">Altclan streamlines brand identity management by automatically collecting, storing, and distributing design tokens and assets. Say goodbye to manual organization – with Altclan, your brand elements are effortlessly centralized and readily accessible, ensuring consistency and coherence across all your projects and platforms. 
+      Simplify your workflow and elevate your brand presence with Altclan today.</p>
     </div>
 
     <div class="mt-6 flex items-center justify-center gap-4">
-      <a href="#" class="transform rounded-md bg-black px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Get started for free</a>
+      <a href="#" class="transform rounded-md bg-black px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Get started</a>
       <a href="#" class="transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"> Request a demo </a>
     </div>
   </div>
