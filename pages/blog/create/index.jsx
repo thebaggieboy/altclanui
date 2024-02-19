77a8bf3410319e8e651630a0af856b1619b2e37a
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-02-19 07:32:47 +0100

    Changing fonts to Poppins sans-serif

---
+++ pages/blog/create/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from '../../../styles/brand-bio.module.css'
+export default function Create() {
+    const inputChangeHandler = (e) => {
+        const { name, value } = e.target
+        setFormData((prevValue) => {
+          return {
+            ...prevValue,
+            [name]: value
+          }
+        })
+        console.log("Form Data: ", formData)
+      }
+    
+  return (
+    <>
+    <section style={{padding:20}}>
+        <div>
+            <label htmlFor="" className={styles.label}>Article Title</label>
+            <input type="text" onChange={inputChangeHandler} name="merchandise_name" id="brand-name" className={styles.input} placeholder="" required />
+
+          </div>
+          <div>
+            <label htmlFor="" className={styles.label}>Details</label>
+            <textarea type="text" onChange={inputChangeHandler} name="merchandise_name" id="brand-name" className={styles.input} placeholder="" required></textarea>
+
+          </div>
+        </section>
+    </>
+  )
+}
