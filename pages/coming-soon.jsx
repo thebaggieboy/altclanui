f4f355a3323e9954f76cc33aac815ebfa3b56bc8
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-02-19 14:14:58 +0100

    dknfr

--- pages/coming-soon.jsx
+++ pages/coming-soon.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ComingSoon from '../components/ComingSoon'
 
-export default function 
-() {
+export default function ComingSoon() {
   return (
     <>
         <ComingSoon/>
