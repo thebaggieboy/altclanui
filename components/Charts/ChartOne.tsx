df198ec771cd961d86b2d39e43faa9cd8fc5eb2f
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-03-17 04:20:20 +0100

    dd

--- components/Charts/ChartOne.tsx
+++ components/Charts/ChartOne.tsx
@@ -134,13 +134,10 @@ interface ChartOneState {
 const ChartOne: React.FC = () => {
   const [state, setState] = useState<ChartOneState>({
     series: [
-      {
-        name: "Product One",
-        data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
-      },
+   
 
       {
-        name: "Product Two",
+        name: "Products",
         data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
       },
     ],
--- pages/brands/dashboard/[id].tsx
+++ pages/brands/dashboard/[id].tsx
@@ -190,13 +190,21 @@ const ECommerce = ({merch}) => {
       <div className="mt-4 p-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
+     
+
+      {/**   <MapOne /> */}
+   
+   {/** <ChatCard />*/}
+   <div className="col-span-12 xl:col-span-8">
+      <ChartThree />
         </div>
-        <ChatCard />
+      <div className="col-span-8 xl:col-span-8">
+           {/** <TableOne /> */}
+    
+        </div>
+        
       </div>
+     
     </>
   );
 };
