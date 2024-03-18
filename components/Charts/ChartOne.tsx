47ee3260b5d5c353c9027fc5d82596118d3bfb46
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-03-18 12:06:05 +0100

    Changed the aesthetic component

--- components/Charts/ChartOne.tsx
+++ components/Charts/ChartOne.tsx
@@ -160,15 +160,7 @@ const ChartOne: React.FC = () => {
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
       <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
         <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-          <div className="flex min-w-47.5">
-            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
-              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
-            </span>
-            <div className="w-full">
-              <p className="font-semibold text-primary">Total Revenue</p>
-              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
-            </div>
-          </div>
+         
           <div className="flex min-w-47.5">
             <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-secondary">
               <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-secondary"></span>
--- pages/brands/dashboard/[id].tsx
+++ pages/brands/dashboard/[id].tsx
@@ -96,8 +96,8 @@ const ECommerce = ({merch}) => {
   return (
     
     <>
-  <h1 className="mt-5 p-2 bolder text-center" style={{fontFamily:'Poppins, Sans-serif', fontSize:20, fontWeight:"bolder"}}>Welcome {user?.brand_name}</h1>
-      <div className="mt-4 p-4 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+  
+      <div className="mt-2 p-4 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <CardDataStats title="Total Revenue" total={total === undefined ? 0 : `₦${total}`} rate="0.00%">
           <svg
             className="fill-primary dark:fill-white"
