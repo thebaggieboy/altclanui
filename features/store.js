6b4710e0d56ade1ed16b3c9bad289b99e9354d4e
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-03-19 18:44:59 +0100

    efjby

--- features/store.js
+++ features/store.js
@@ -21,7 +21,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ["shop", "brands", "user",]
+    whitelist: ["shop", "brands", "user"]
 }
 
 const rootReducer = combineReducers({
