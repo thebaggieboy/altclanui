57a4255051d7491056ec3c9a3619fa3389f2575a
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-03-19 18:54:01 +0100

    i believe i have achieved natural flow

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -61,7 +61,7 @@ export default function Blog() {
       </div> 
       <div class="grid gap-8 lg:grid-cols-2 p-5">
       {data.map((article)=>(
-       <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+       <article key={article.id} class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
        <div class="flex justify-between items-center mb-5 text-gray-500">
            <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
