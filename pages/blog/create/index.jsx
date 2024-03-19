196385044b427712b53972bc5b2ab2b61e4ad969
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-03-19 15:33:42 +0100

    ufbreubf

--- pages/blog/create/index.jsx
+++ pages/blog/create/index.jsx
@@ -3,6 +3,12 @@ import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 import * as commands from "@uiw/react-md-editor/commands"
+import { useDispatch, useSelector } from "react-redux";
+import { USER_TYPES, selectUser, selectUserType, setUser } from "../../../features/user/userSlice";
+import { useRouter } from "next/router";
+
+import styles from '../../../styles/brand-bio.module.css'
+import useBlog from '../../../hooks/useBlog';
 
 
 const MDEditor = dynamic(
@@ -12,10 +18,17 @@ const MDEditor = dynamic(
   
 
 
-import styles from '../../../styles/brand-bio.module.css'
 export default function Create() {
   const [value, setValue] = useState("**Hello world!!!**");
-  
+  const [formData, setFormData] = useState({
+		title: "",
+	})
+  const user = useSelector(selectUser)
+  const router = useRouter();
+const isBrand = useSelector(selectUserType) === USER_TYPES.brand
+
+
+
   const submit = (e)=>{
     e.preventDefault()
     console.log('Article submit button clicked')
@@ -37,6 +50,11 @@ export default function Create() {
     <>
     <section style={{padding:20}}>
       <form onSubmit={submit}>
+
+      <div style={{textAlign:'center', border:0, borderWidth:0}}>
+        <input onChange={inputChangeHandler} type="text" name='title' style={{ border:'none', borderWidth:0, padding:10}} placeholder='Article title' />
+      </div>
+
       <div data-color-mode="light" className='p-5'>
         <div className="wmde-markdown-var"> </div>
 
