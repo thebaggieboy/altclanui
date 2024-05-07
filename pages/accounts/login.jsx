d397c9f440f122b39162b2388f9da7e0405a2c5a
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-05-07 15:44:33 +0100

    ss

--- pages/accounts/login.jsx
+++ pages/accounts/login.jsx
@@ -84,6 +84,7 @@ export default function SignUp() {
 	}
 	function loginEmail(){
 		dispatch(setUserEmail(formData?.email))
+
 	}
 
 
@@ -186,18 +187,6 @@ export default function SignUp() {
 						<div className="">
 							{/* <label for="email" className="block mb-2 text-sm font-medium text-black">Your email</label> */}
 							{error !== null && (
-								<div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-									<svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-										<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-									</svg>
-									<span class="sr-only">Info</span>
-									<div class="ml-3 text-sm text-center font-medium">
-										{formErr?.non_field_errors}
-									</div>
-								
-								</div>
-							)}
-								{formErr !== null && (
 								<div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
 									<svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 										<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
