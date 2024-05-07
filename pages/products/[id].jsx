14655fd72063945386c9caa78a1e8defd41d378e
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-05-07 18:22:13 +0100

    d

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -65,8 +65,14 @@ export default function ProductDetail({ _id, merch }) {
 	const [selectedSize, setSelectedSize] = useState(merch.available_sizes[1]);
 	const { selectedProducts, setSelectedProducts } = useContext(ProductContext);
 	const [open, setOpen] = useState(false);
-	
+	const [added, setAdded] = useState(false);
+	const [successText, setSuccessText] = useState("")
 	function addToCart() {
+		setSuccessText("Item added to cart")
+		setAdded(true)
+
+		console.log(added)
+		console.log(successText)
 		const data = {
 			itemId: merch.id,
 			size: selectedSize,
@@ -115,14 +121,25 @@ export default function ProductDetail({ _id, merch }) {
     </div>
                 <div class="flex -mx-2 mb-4">
                     <div class="w-1/2 px-2">
-                        <button onClick={addToCart} class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Add to Cart</button>
+                        <button onClick={addToCart} class="w-full bg-gray-900 dark:bg-gray-600 text-white py-4 px-4  font-bold hover:bg-black dark:hover:bg-gray-700">Add to Cart</button>
                     </div>
                     <div class="w-1/2 px-2">
-                        <button class="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Add to Wishlist</button>
+                        <button style={{fontWeight:"bold", backgroundColor:"beige", border:"1px solid black"}} class="w-full  dark:bg-gray-700 text-black dark:text-white py-4 px-4 font-bold  dark:hover:bg-gray-600">Add to Wishlist</button>
                     </div>
                 </div>
             </div>
             <div class="md:flex-1 px-4">
+				{added == true ? <div class="flex items-center text-center p-4 mb-4 text-sm text-green-800 border border-1 bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
+				<svg style={{fontFamily:"Poppins, Sans-Serif"}} class="w-6 h-6 font-bold text-green-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path d="M12.268 6A2 2 0 0 0 14 9h1v1a2 2 0 0 0 3.04 1.708l-.311 1.496a1 1 0 0 1-.979.796H8.605l.208 1H16a3 3 0 1 1-2.83 2h-2.34a3 3 0 1 1-4.009-1.76L4.686 5H4a1 1 0 0 1 0-2h1.5a1 1 0 0 1 .979.796L6.939 6h5.329Z"/>
+  <path d="M18 4a1 1 0 1 0-2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0V8h2a1 1 0 1 0 0-2h-2V4Z"/>
+</svg>
+
+		<span class="sr-only">Tick</span>
+		<div className="ml-5">
+		{successText}
+		</div>
+	  </div> : ""}
                 <h2 style={{fontFamily:"Poppins, Sans-Serif"}} class="text-4xl text-black font-bold dark:text-white mb-2">{merch.merchandise_name}</h2>
 				  <h6 style={{fontFamily:"Poppins, Sans-Serif", backgroundColor:"beige", padding:10, border:"1px solid black", width:140, fontSize:14, fontWeight:"bold", fontStyle:"oblique"}} class="text-black dark:text-white mb-2">{merch.brand_name}</h6>
                 <p class="text-gray-600 dark:text-gray-300 text-sm mb-4">
@@ -135,7 +152,7 @@ export default function ProductDetail({ _id, merch }) {
                     </div>
                     <div>
                         <span class=" text-gray-700 dark:text-gray-300">Availability:</span>
-                        <span class="text-green-700 font-bold text-sm dark:text-gray-300">In Stock</span>
+                        <span class="text-green-700 font-bold text-sm dark:text-gray-300"> In Stock</span>
                     </div>
                 </div> 
 
