6dd588b925a5b6094d5fd013d5b2245769417c3d
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-06-26 19:18:49 +0100

    s

--- pages/api/emails/index.js
+++ pages/api/emails/index.js
@@ -1,14 +1,18 @@
 import { Resend } from 'resend';
 import {Welcome} from "../../../emails/Welcome"
 
-export async function POST(){
+export async function POST(req, res){
     const resend = new Resend('re_EBX5ss1T_9n7aStcnsnfBES3TsZdXmiEy');
 			
-    await resend.emails.send({
+    const { data, error } =  await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: 'enimofeodujirin@gmail.com',
       subject: 'hello world',
       react: Welcome(),
     });
-
+    if (error) {
+        return res.status(400).json(error);
+      }
+    
+      res.status(200).json(data);
 }
